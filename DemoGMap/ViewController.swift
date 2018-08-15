a4c07fc021462a08be154848ec9cb1cabdd9e8a5
Đừng xóa <nomnom1204@gmail.com>
2018-08-15 10:32:59 +0700

    + show Direction

--- DemoGMap/ViewController.swift
+++ DemoGMap/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+typealias DICT = Dictionary<AnyHashable, Any>
 
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var pinImageVerticalConstraint: NSLayoutConstraint!
     private let locationManager = CLLocationManager()
+    var searchLocation: CLLocationCoordinate2D?
+    var currentLocation: CLLocationCoordinate2D?
+    
+    let maker = GMSMarker()
+    var oldPolylineArr = GMSPolyline()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +46,11 @@ class ViewController: UIViewController {
         let autocompleteController = GMSAutocompleteViewController()
         autocompleteController.delegate = self
         present(autocompleteController, animated: true, completion: nil)
+    }
+    
+    @IBAction func deleteMarker(_ sender: UIButton) {
+        maker.map = nil
+        oldPolylineArr.map = nil
     }
     
     
@@ -70,7 +82,7 @@ class ViewController: UIViewController {
 }
 
 
-    // a extension that conforms to CLLocationManagerDelegate
+// a extension that conforms to CLLocationManagerDelegate
 extension ViewController: CLLocationManagerDelegate {
     // called when the user grants or revokes location permissions.
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -92,8 +104,8 @@ extension ViewController: CLLocationManagerDelegate {
         
         //updates the map’s camera to center around the user’s current location.
         mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
-        let locValueL: CLLocationCoordinate2D = (manager.location?.coordinate)!
-        print("locations: \(locValueL.latitude), \(locValueL.longitude)")
+        currentLocation = (manager.location?.coordinate)!
+//        print("1 locations: \(String(describing: currentLocation?.latitude)), \(String(describing: currentLocation?.longitude))")
         locationManager.stopUpdatingLocation()
     }
 }
@@ -108,10 +120,24 @@ extension ViewController: GMSMapViewDelegate {
 
 extension ViewController: GMSAutocompleteViewControllerDelegate {
     func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        maker.map = nil
         print("Place name: \(place.name)")
         print("Place address: \(place.formattedAddress ?? "")")
-        print("Place attributions: \(String(describing: place.attributions))")
+//        print("Place attributions: \(String(describing: place.attributions))")
+        searchLocation = place.coordinate
+        
+//        searchLocLatitude = place.coordinate.longitude
+//        searchLocLongitude = place.coordinate.longitude
+        mapView.camera = GMSCameraPosition.camera(withLatitude: (searchLocation?.latitude)!, longitude: (searchLocation?.longitude)!, zoom: 15)
+        showMaker(position: mapView.camera.target)
+//        print("2 locations: \(String(describing: searchLocation?.latitude)), \(String(describing: searchLocation?.longitude))")
+        getPolylineRoute(from: currentLocation!, to: searchLocation!)
         dismiss(animated: true, completion: nil)
+    }
+    
+    func showMaker(position: CLLocationCoordinate2D) {
+        maker.position = position
+        maker.map = mapView
     }
     
     func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
@@ -128,6 +154,44 @@ extension ViewController: GMSAutocompleteViewControllerDelegate {
     
     func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+    func getPolylineRoute(from source : CLLocationCoordinate2D, to destination: CLLocationCoordinate2D) {
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        let url = URL(string: "https://maps.googleapis.com/maps/api/directions/json?origin=\(source.latitude),\(source.longitude)&destination=\(destination.latitude),\(destination.longitude)&mode=driving&units=metric")!
+        
+        let task = session.dataTask(with: url, completionHandler: { (data, response, error) in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            guard let aData = data else {return}
+            do {
+                if let json = try JSONSerialization.jsonObject(with: aData, options: .mutableContainers) as? DICT {
+                    let routes = json["routes"] as? [DICT] ?? []
+                    let zero = routes[0]
+                    let overview_polyline = zero["overview_polyline"] as? DICT ?? [:]
+                    let points = overview_polyline["points"] as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self.showPath(polyStr: points)
+                    }
+                }
+            } catch {
+                print("error in JSONSerialization")
+            }
+        })
+        task.resume()
+    }
+    
+    func showPath(polyStr: String) {
+        oldPolylineArr.map = nil
+        let path = GMSPath(fromEncodedPath: polyStr)
+        let polyline = GMSPolyline(path: path)
+        polyline.strokeWidth = 3.0
+        polyline.map = mapView
+        oldPolylineArr = polyline
     }
 }
 
@@ -162,3 +226,4 @@ extension ViewController: GMSAutocompleteViewControllerDelegate {
 //        return view
 //    }
 //}
+
